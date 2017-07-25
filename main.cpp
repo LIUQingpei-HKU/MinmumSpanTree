f098720aae1dac4d9d8c2221fa964671aff8ed5d
LIUQingpei-HKU <liuqingp@hku.hk>
2017-07-25 12:38:08 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <iomanip>
+#include "DenseGraph.h"
+#include "SparseGraph.h"
+#include "ReadGraph.h"
+#include "LazyPrimMST.h"
+#include "PrimMST.h"
+#include "KruskalMST.h"
+
+using namespace std;
+
+// 测试有权图和有权图的读取
+int main() {
+//
+//    string filename = "testG1.txt";
+//    int V = 8;
+//    cout<<fixed<<setprecision(2);
+//
+//    // Test Weighted Dense Graph
+//    DenseGraph<double> g1 = DenseGraph<double>(V,false);
+//    ReadGraph<DenseGraph<double>,double> readGraph1(g1,filename);
+//    g1.show();
+//    cout<<endl;
+//
+//    // Test Weighted Sparse Graph
+//    SparseGraph<double> g2 = SparseGraph<double>(V, false);
+//    ReadGraph<SparseGraph<double>,double> readGraph2(g2,filename);
+//    g2.show();
+//    cout<<endl;
+    string filename = "testG1.txt";
+    int V = 8;
+
+    SparseGraph<double> g = SparseGraph<double>(V, false);
+    ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
+
+    // Test Lazy Prim MST
+    cout<<"Test Lazy Prim MST:"<<endl;
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
+    vector<Edge<double>> mst = lazyPrimMST.mstEdges();
+    for( int i = 0 ; i < mst.size() ; i ++ )
+        cout<<mst[i]<<endl;
+    cout<<"The MST weight is: "<<lazyPrimMST.result()<<endl;
+
+    cout<<endl;
+
+    // Test Prim MST
+    cout<<"Test Prim MST:"<<endl;
+    PrimMST<SparseGraph<double>, double> primMST(g);
+    mst = primMST.mstEdges();
+    for( int i = 0 ; i < mst.size() ; i ++ )
+        cout<<mst[i]<<endl;
+    cout<<"The MST weight is: "<<primMST.result()<<endl;
+
+    cout<<endl;
+
+    //Test Kruskal MST
+    cout<<"Test Kruskal MST:"<<endl;
+    KruskalMST<SparseGraph<double>,double> kruskalMST(g);
+    mst = kruskalMST.mstEdges();
+    for(int i=0;i<mst.size();i++)
+        cout<<mst[i]<<endl;
+    cout<<"The MST weight is: "<<kruskalMST.result();
+
+    return 0;
+}
